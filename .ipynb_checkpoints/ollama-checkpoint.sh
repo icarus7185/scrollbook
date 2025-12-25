c5369e75039a68694aa9af2cdbde95a48839729c
icarus7185 <163373986+icarus7185@users.noreply.github.com>
2025-12-25 07:42:39 +0700

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/ollama-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-export OLLAMA_MODELS="/workspace/models/ollama"
-curl -fsSL https://ollama.com/install.sh | sh
-ollama serve
--- .ipynb_checkpoints/ollama-pull-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-export OLLAMA_MODELS="/workspace/models/ollama"
-ollama pull llama3.2:1b
-ollama pull mxbai-embed-large
