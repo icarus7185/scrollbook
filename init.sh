208a01584d2dbecb432559d75de9fb4d6ee963c0
icarus7185 <icarus7185@gmail.com>
2025-12-25 07:15:13 +0000

    update

--- init.sh
+++ init.sh
@@ -1,19 +1,7 @@
-# this is to fool github
-gh1=mpcQLStUfCMdxhbg
-gh2=k5vVWfhGdV7nG144GH6l
-gh=ghp_${gh1}${gh2}
-
-# login github
-apt update
-apt install gh
-echo ${gh} | gh auth login --with-token
-gh auth setup-git
-git config --global user.email "icarus7185@gmail.com"
-git config --global user.name "icarus7185"
+# add variable
+echo 'export OLLAMA_MODELS="/workspace/models/ollama"' | tee -a ~/.bashrc > /dev/null
+source ~/. bashrc
 
 # active conda
 source /workspace/miniconda3/bin/activate
 conda init bash
-
-# add variable
-echo 'export OLLAMA_MODELS="/workspace/models/ollama"' | tee -a ~/.bashrc > /dev/null
--- ollama-pull.sh
+++ ollama-pull.sh
@@ -1,3 +1,3 @@
 export OLLAMA_MODELS="/workspace/models/ollama"
-ollama pull llama3.2:1b
+ollama pull llama3.2:3b
 ollama pull mxbai-embed-large
